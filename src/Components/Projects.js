a2b141a35531a69f364534bb6e994d75f855a8e8
Wali Haider <109701526+wali15@users.noreply.github.com>
2023-01-12 17:54:42 +0500

    our project components added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,9 +1,37 @@
 import React from "react";
+import "./Project.css";
+import AbdullahMallPic1 from "../OurProjectsImages/AbdullahMallPic1.jpg";
+import AbdullahMallPic2 from "../OurProjectsImages/AbdullahMallPic2.jpg";
+import AbdullahMallPic3 from "../OurProjectsImages/AbdullahMallPic3.jpg";
 
 const Projects = () => {
   return (
     <div className="container">
       <h1>Our Projects</h1>
+      <h4>Abdullah Mall (Hyderabad)</h4>
+      <div className="row-center">
+        <img src={AbdullahMallPic1} class="img" alt="Abdullah Mall" />
+        <img src={AbdullahMallPic2} class="img" alt="Abdullah Mall" />
+        <img src={AbdullahMallPic3} class="img" alt="Abdullah Mall" />
+      </div>
+      <h4>Abdullah Mall (Hyderabad)</h4>
+      <div className="row-center">
+        <img src={AbdullahMallPic1} class="img" alt="Abdullah Mall" />
+        <img src={AbdullahMallPic2} class="img" alt="Abdullah Mall" />
+        <img src={AbdullahMallPic3} class="img" alt="Abdullah Mall" />
+      </div>
+      <h4>Abdullah Mall (Hyderabad)</h4>
+      <div className="row-center">
+        <img src={AbdullahMallPic1} class="img" alt="Abdullah Mall" />
+        <img src={AbdullahMallPic2} class="img" alt="Abdullah Mall" />
+        <img src={AbdullahMallPic3} class="img" alt="Abdullah Mall" />
+      </div>
+      <h4>Abdullah Mall (Hyderabad)</h4>
+      <div className="row-center">
+        <img src={AbdullahMallPic1} class="img" alt="Abdullah Mall" />
+        <img src={AbdullahMallPic2} class="img" alt="Abdullah Mall" />
+        <img src={AbdullahMallPic3} class="img" alt="Abdullah Mall" />
+      </div>
     </div>
   );
 };
